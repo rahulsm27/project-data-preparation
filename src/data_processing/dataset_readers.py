e44f12221578e0999b7968dfa12d35d159dad234
Rahul <rahulsm.27@gmail.com>
2023-12-04 23:26:01 +0530

    Final changes for GCP

--- src/data_processing/dataset_readers.py
+++ src/data_processing/dataset_readers.py
@@ -6,7 +6,7 @@ from typing import Optional
 import dask.dataframe as dd
 
 from dask_ml.model_selection import train_test_split
-
+from dvc.api import get_url
 from src.utils.utils import get_loggerf
 from src.utils.data_utils import get_repo_address_with_access_token
 from src.utils.data_utils import repartition_dataframe
@@ -15,13 +15,13 @@ class DatasetReader(ABC):
     required_columns = {"text", "label", "split", "dataset_name"}
     split_names = {"train", "dev", "test"}
 
-    def __init__(self, dataset_dir: str, dataset_name: str, gcp_project_id: str,gcp_github_access_token_secret_id: str, dvc_remote_repo: str, github_user_name: str,version: str,) -> None:
+    def __init__(self, dataset_dir: str, dataset_name: str, gcp_project_id: str,gcp_github_access_token_secret_id: str, dvc_remote_repo: str, github_user_name: str,version: str) -> None:
         self.logger = get_logger(self.__class__.__name__)
         self.dataset_dir = dataset_dir
         self.dataset_name = dataset_name
-        # self.dvc_remote_repo = get_repo_address_with_access_token(
-        #     gcp_project_id, gcp_github_access_token_secret_id, dvc_remote_repo, github_user_name
-        # )
+        self.dvc_remote_repo = get_repo_address_with_access_token( gcp_project_id, gcp_github_access_token_secret_id, dvc_remote_repo, github_user_name
+         )
+        self.version = version
 
     def read_data(self) -> dd.core.DataFrame:
         train_df, dev_df, test_df = self._read_data()
@@ -68,7 +68,7 @@ class DatasetReader(ABC):
             return first_df, second_df 
 
     def get_remote_data_url(self, dataset_path: str) -> str:
-        dataset_url: str = "ABC" #get_url(path=dataset_path, repo=self.dvc_remote_repo, rev=self.version)
+        dataset_url: str = get_url(path=dataset_path, repo=self.dvc_remote_repo, rev=self.version)
         return dataset_url
 class GHCDatasetReader(DatasetReader):
     def __init__(self, dataset_dir: str, dataset_name: str, dev_split_ratio: float,gcp_project_id: str,
@@ -86,9 +86,11 @@ class GHCDatasetReader(DatasetReader):
     def _read_data(self) -> tuple:
         self.logger.info("Reading GHC data")
         train_tsv_path = os.path.join(self.dataset_dir, "ghc_train.tsv")
-        train_df = dd.read_csv(train_tsv_path, sep="\t", header=0)
+        train_tsv_url = self.get_remote_data_url(train_tsv_path)
+        train_df = dd.read_csv(train_tsv_url, sep="\t", header=0)
         test_tsv_path = os.path.join(self.dataset_dir, "ghc_test.tsv")
-        test_df = dd.read_csv(test_tsv_path, sep="\t", header=0)
+        test_tsv_url = self.get_remote_data_url(test_tsv_path)
+        test_df = dd.read_csv(test_tsv_url, sep="\t", header=0)
 
         train_df["label"] = ((train_df["hd"] + train_df["cv"] + train_df["vo"]) > 0).astype(int)
 
